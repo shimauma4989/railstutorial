295c4451117f6a6406aad5f677ce5e94a4c54cd1
ironsand <eisensand@gmail.com>
2013-11-18 10:30:08 +0900

    fix sign out spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,10 +52,6 @@ describe "UserPages" do
         it {should have_title(user.name)}
         it {should have_selector('div.alert.alert-success',text:'Welcome')}
       end
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
-      end
     end
   end
 end
